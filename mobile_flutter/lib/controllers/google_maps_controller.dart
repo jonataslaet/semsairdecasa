8a9952e809ccdfd05aa2d274be38bd03cc13c8be
jonataslaet <jonataslaetprogramador@gmail.com>
2023-05-21 13:44:30 -0300

    Inclusão do google maps
    A view do mapa foi setada como página inicial

---
+++ mobile_flutter/lib/controllers/google_maps_controller.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class GoogleMapsController extends StatefulWidget {
+  const GoogleMapsController({super.key});
+
+  @override
+  State<GoogleMapsController> createState() =>
+      _GoogleMapsControllerState();
+}
+
+class _GoogleMapsControllerState extends State<GoogleMapsController> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(-5.08921, -42.80165);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(target: _center, zoom: 11.0,),
+        )
+      ),
+    );
+  }
+}
---
+++ mobile_flutter/lib/views/maps_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_flutter/controllers/google_maps_controller.dart';
+
+class MapsView extends StatelessWidget {
+  const MapsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mapa'),
+      ),
+      body: const Center(
+        child: GoogleMapsController()
+      ),
+    );
+  }
+}
